832d507e309366ca8e93b31c7e55115a390c1e6c
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-14 16:14:33 +0100

    CoachItem component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,16 +6,26 @@
       <router-link to="/register">Register As Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+      v-for="coach in filteredCoaches"
+      :key="coach.id"
+      :first-name="coach.firstName"
+      :last-name="coach.lastName"
+      :rate="coach.hourlyRate"
+      :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found...</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem'
+
 export default {
+  components: {
+    CoachItem
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches']
@@ -27,6 +37,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
