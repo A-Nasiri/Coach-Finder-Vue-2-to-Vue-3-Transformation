d240f57e3890d6f5469253bf0e0bd8ce5da02715
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-17 19:28:46 +0100

    Storing messages in Vuex

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -31,6 +31,14 @@ export default {
         this.formIsValid = false;
         return;
       }
+
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.id
+      })
+
+      this.$router.replace('/coaches');
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index';
+import requestsModule from './modules/requests/index';
 
 const store = createStore({
     modules: {
-        coaches: coachesModule
+        coaches: coachesModule,
+        requests: requestsModule
     },
     state() {
         return {
