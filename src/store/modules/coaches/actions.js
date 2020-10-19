cc57d095f02dabdc1d0994f034a3a1704e876fcd
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-19 10:54:34 +0100

    Fetching coaches data from the backend

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -24,5 +24,32 @@ export default {
             ...coachData,
             id: userId
         });
+    },
+
+    async loadCoaches(context) {
+        const response = await fetch(
+            `https://coach-finder-abe52.firebaseio.com/coaches.json`
+        );
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            // bla bla
+        }
+        
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas
+            }
+            coaches.push(coach);
+        }
+
+        context.commit('setCoaches', coaches);
     }
 };
