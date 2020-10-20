7b4665ebfe2bf7b395e73c7dbd20138a7fa9d5ae
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-20 19:57:08 +0100

    Caching response data from backend on client

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,7 +26,11 @@ export default {
         });
     },
 
-    async loadCoaches(context) {
+    async loadCoaches(context, payload) {
+        if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+            return;
+        }
+
         const response = await fetch(
             `https://coach-finder-abe52.firebaseio.com/coaches.json`
         );
@@ -53,5 +57,6 @@ export default {
         }
 
         context.commit('setCoaches', coaches);
+        context.commit('setFetchTimestamp');
     }
 };
