179fd968a64a52e90f4b02c6c36d7625dfd6b551
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-21 23:41:39 +0100

    Not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+      <base-card>
+        <h2>Page not found!</h2>
+        <base-button link to="/coaches">Find a Mentor</base-button>
+      </base-card>
+  </section>
+</template>
+
