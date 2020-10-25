93fac506407419d51635910e078e346a2cd3ce79
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-25 15:06:44 +0000

    Logout action & flow

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -8,6 +8,7 @@
               <li><router-link to="/coaches">All Coaches</router-link></li>
               <li v-if="isLoggedIn"><router-link to="/requests">Requests</router-link></li>
               <li v-else><router-link to="/auth">Login</router-link></li>
+              <li v-if="isLoggedIn"><base-button @click="logout">Logout</base-button></li>
           </ul>
       </nav>
   </header>
@@ -18,6 +19,11 @@ export default {
   computed: {
     isLoggedIn() {
       return this.$store.getters.isAuthenticated;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
     }
   }
 }
