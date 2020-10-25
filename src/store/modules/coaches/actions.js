7237f4e72219f6938eae671d97d34fac35e98399
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-25 03:51:33 +0000

    Adding user token to outgoing requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,7 +9,10 @@ export default {
             areas: data.areas
         }
 
-        const response = await fetch(`https://coach-finder-abe52.firebaseio.com/coaches/${userId}.json`, {
+        const token = context.rootGetters.token;
+
+        const response = await fetch(`https://coach-finder-abe52.firebaseio.com/coaches/${userId}.json?auth=` + 
+        token, {
             method: 'PUT',
             body: JSON.stringify(coachData)
         });
