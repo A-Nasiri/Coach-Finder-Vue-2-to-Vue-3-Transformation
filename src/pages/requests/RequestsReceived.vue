c4a389810aeb72009ac4392237293c48fc205b04
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2020-10-29 02:07:08 +0000

    Composition-API migration: RequestsReceived.vue

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -22,43 +22,84 @@
 </template>
 
 <script>
+import { ref, computed } from 'vue';
+import { useStore } from 'vuex';
 import RequestItem from '../../components/requests/RequestItem';
 
 export default {
   components: {
     RequestItem
   },
-  data() {
-    return {
-      isLoading: false,
-      error: null
-    }
-  },
-  computed: {
-    receivedRequests() {
-      return this.$store.getters['requests/requests'];
-    },
-    hasRequests() {
-      return this.$store.getters['requests/hasRequests'];
-    }
-  },
-  created() {
-    this.loadRequests();
-  },
-  methods: {
-    async loadRequests() {
-      this.isLoading = true;
+  setup() {
+    const store = useStore();
+
+    const isLoading = ref(false);
+    const error = ref(null);
+
+    loadRequests();
+
+    const receivedRequests = computed(() => {
+      return store.getters['requests/requests'];
+    });
+
+    const hasRequests = computed(() => {
+      return store.getters['requests/hasRequests'];
+    });
+
+    async function loadRequests() {
+      isLoading.value = true;
       try {
-        await this.$store.dispatch('requests/fetchRequests');
-      } catch (error) {
-        this.error = error.message || 'Something failed!'
+        await store.dispatch('requests/fetchRequests');
+      } catch (err) {
+        error.value = err.message || 'Something failed!'
       }
-      this.isLoading = false;
-    },
-    handleError() {
-      this.error = null;
+      isLoading.value = false;
     }
-  }
+
+    function handleError() {
+      error.value = null;
+    }
+
+    return {
+      isLoading,
+      error,
+      receivedRequests,
+      hasRequests,
+      loadRequests,
+      handleError
+    }
+  },
+  // data() {
+  //   return {
+  //     isLoading: false,
+  //     error: null
+  //   }
+  // },
+  // computed: {
+  //   receivedRequests() {
+  //     return this.$store.getters['requests/requests'];
+  //   },
+  //   hasRequests() {
+  //     return this.$store.getters['requests/hasRequests'];
+  //   }
+  // },
+  // created() {
+  //   this.loadRequests();
+  // },
+  // methods: {
+  //   async loadRequests() {
+  //     this.isLoading = true;
+  //     try {
+  //       await this.$store.dispatch('requests/fetchRequests');
+  //     } catch (error) {
+  //       this.error = error.message || 'Something failed!'
+  //     }
+  //     this.isLoading = false;
+  //   },
+  //   handleError() {
+  //     this.error = null;
+  //   }
+  // }
 }
 </script>
 
